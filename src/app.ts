19457bd014f559bf4e1499021f6c908b2183b831
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2024-11-10 22:42:21 -0500

    hashed password using bcrypt

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 
 import { envs } from "./config";
-import { MongoDatabase } from "./data";
+import { MongoDatabase } from "./data/mongodb";
 
 import { AppRoutes } from "./presentation/routes";
 import { Server } from "./presentation/server"
--- src/infraestructure/datasources/auth.datasource.impl.ts
+++ src/infraestructure/datasources/auth.datasource.impl.ts
@@ -1,20 +1,47 @@
+import { BcryptAdapter } from "../../config";
+import { UserModel } from "../../data/mongodb";
 import { AuthDatasource, CustomError, RegisterUserDto, UserEntity } from "../../domain";
 
+
+type HashFunction = (password: string) => string;
+type compareFunction = (password: string, hashed: string) => boolean;
+
 export class AuthDatasourceImpl implements AuthDatasource {
+
+  constructor(
+    private readonly hashFunction: HashFunction = BcryptAdapter.hash,
+    private readonly compareFunction: compareFunction = BcryptAdapter.compare
+  ) { }
+
   async register(registerUserDto: RegisterUserDto): Promise<UserEntity> {
 
     const { name, email, password } = registerUserDto;
+
     try {
       //1. Verificar si el correo existe
+      const emailExists = await UserModel.findOne({ email: email });
+      if (emailExists) throw CustomError.badRequest('Email already exists');
+
+      const user = await UserModel.create({
+        name: name,
+        email: email,
+        password: this.hashFunction(password),
+      });
+
+      await user.save();
+
       //2. Hashear la constraseña
+
       //3. Mapear la data con nuestra entidad
+      //Todo: el mapper
+
 
       return new UserEntity(
-        '1',
+        user.id,
         name,
         email,
-        password,
-        ['ADMIN_ROLE'],
+        user.password,
+        user.roles,
       );
 
 
